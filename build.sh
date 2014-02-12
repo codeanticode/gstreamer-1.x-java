1ee85cb319a43d5af98ace19d39a391cced7b583
sw360cab <sw360cab@gmail.com>
2014-02-12 16:01:58 +0100

    modified with new config files

--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
 
-java -jar jnaerator.jar config.jnaerator
+jnaerate (){
+ java -jar jnaerator.jar $1
+ #echo "cccc"
+ if [ $? -eq 0 ] 
+ then 
+   echo "jnaeration using $1 succeded"
+   return 0
+ else
+   echo "jnaeration using $1 failed"
+   return 1
+ fi 
+}  
 
+jnaerate gobject.jnaerator
+if [ $? -eq 0 ] 
+then 
+  jnaerate gstreamer.jnaerator
+else
+  echo "exiting because of error in gobject.jnaerator"
+fi
