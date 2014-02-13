f1dccfd459b7b4e7012271706e639f59e3b3073a
sw360cab <sw360cab@gmail.com>
2014-02-13 11:41:44 +0100

    new configuration done

---
+++ gstreamer/_stdint.h
@@ -0,0 +1,9 @@
+#ifndef _GSTREAMER__STDINT_H
+#define _GSTREAMER__STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "gstreamer 1.2.2"
+/* generated using gnu compiler gcc (Ubuntu/Linaro 4.8.1-10ubuntu9) 4.8.1 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
