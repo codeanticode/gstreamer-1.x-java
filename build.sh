a25f90f69c3d048b19731a2d71a6febbdfaf8f30
codeanticode <andres.colubri@gmail.com>
2014-02-28 17:04:34 -0500

    added gio

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 java -jar jnaerator.jar glib.jnaerator -mode Directory -f -v
+
+java -jar jnaerator.jar gobject.jnaerator -mode Directory -f -v
+
+java -jar jnaerator.jar gio.jnaerator -mode Directory -f -v
